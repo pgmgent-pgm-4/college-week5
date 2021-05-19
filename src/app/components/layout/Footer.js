9b9ba19a42dcd470f05dbde1fe9081367254036d
Philippe De Pauw - Waterschoot <drdynscript>
2021-05-19 09:38:53 +0200

    Added GitHub Actions Workflow

--- src/app/components/layout/Footer.js
+++ src/app/components/layout/Footer.js
@@ -1,9 +1,13 @@
+import { Link } from "react-router-dom";
+
+import * as Routes from '../../routes';
+
 import styles from './Footer.module.scss';
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      FOOTER
+      <Link to={Routes.FOUROFOUR}>404</Link>
     </footer>
   );
 };
