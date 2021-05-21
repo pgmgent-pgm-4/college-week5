f038e2c8a843e4a9823378a69fd5869060c293a8
Adriaan Glibert <adriaan@dive.be>
2021-05-21 10:12:36 +0200

    🚨 Remove unused components

--- src/app/components/button/Button.js
+++ src/app/components/button/Button.js
@@ -1,6 +1,5 @@
 import React, {useCallback, useState} from 'react'
 
-import AutoFocusableInput from '../input/AutoFocusableInput';
 import BigList from '../list/BigList';
 
 const Button = () => {
--- src/app/pages/HomePage.js
+++ src/app/pages/HomePage.js
@@ -1,6 +1,5 @@
 import { BaseLayout } from '../layouts';
 import Button from '../components/button/Button';
-import Input from '../components/input/AutoFocusableInput';
 import LogButtonClicks from '../components/button/LogButtonClicks';
 import Stopwatch from '../components/stopwatch/Stopwatch';
 
